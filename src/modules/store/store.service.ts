1dd09e5f06d04a2499db7c858fa67bbda2903213
hebertwilly <hebertwilly@hotmail.com>
2026-02-26 23:59:58 -0300

    feat: implementa generateSlug função responsavel por criar os slugs da lojas

--- src/modules/store/store.service.ts
+++ src/modules/store/store.service.ts
@@ -2,7 +2,6 @@ import { Store, StoreDocument } from "./store.model";
 
 export interface CreateStoreDTO {
   name: string;
-  slug: string;
   email: string;
   password: string;
   whatsappNumber: string;
@@ -10,21 +9,43 @@ export interface CreateStoreDTO {
 
 class StoreService {
   async createStore(data: CreateStoreDTO): Promise<StoreDocument> {
+    
     const emailExists = await Store.findOne({ email: data.email });
+    
     if (emailExists) {
       throw new Error("Email já cadastrado");
     }
 
-    const slugExists = await Store.findOne({ slug: data.slug });
-    if (slugExists) {
-      throw new Error("Slug já está em uso");
-    }
+    const slug = await this.generateSlug(data.name);
 
-    const store = await Store.create(data);
+    const store = await Store.create({
+      ...data,
+      slug,
+    });
 
     return store;
   }
 
+  private async generateSlug (name: String): Promise<string> {
+    const baseSlug = name
+      .toLowerCase()
+      .trim()
+      .normalize("NFD")
+      .replace(/[\u0300-\u036f]/g, "")
+      .replace(/\s+/g, "-")
+      .replace(/[^\w-]+/g, "");
+
+    let slug = baseSlug;
+    let counter = 1;
+
+    
+    while (await Store.findOne({ slug })) {
+      slug = `${baseSlug}-${counter}`;
+      counter++;
+    }
+
+    return slug;
+  }
 }
 
 export const storeService = new StoreService();
