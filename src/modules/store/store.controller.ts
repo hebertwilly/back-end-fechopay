f2faf92d3bd196946c615d95f9fa570b03ca1a5f
hebertwilly <hebertwilly@hotmail.com>
2026-03-16 19:55:05 -0300

    feat: implementa a função controller de update store

--- src/modules/store/store.controller.ts
+++ src/modules/store/store.controller.ts
@@ -1,14 +1,34 @@
 import { Request, Response, NextFunction } from "express";
 import { storeService } from "./store.service";
-import { createStoreSchema } from "./store.schema";
-
+import { createStoreSchema, updateStoreSchema } from "./store.schema";
+interface Params {
+  id: string;
+}
 class StoreController {
+
   async create(req: Request, res: Response, next: NextFunction) {
     try {
 
       const validatedData = createStoreSchema.parse(req.body);
       
       const store = await storeService.createStore(validatedData);
+
+      return res.status(201).json({
+        success: true,
+        data: store,
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async update(req: Request<Params>, res: Response, next: NextFunction){
+    try {
+      const validatedData = updateStoreSchema.parse(req.body);
+
+      const { id } = req.params;
+
+      const store = await storeService.updateStore(id, validatedData);
 
       return res.status(201).json({
         success: true,
