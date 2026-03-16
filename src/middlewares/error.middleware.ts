26d8feab2124f3d9afec0582632d0a769b6d399f
hebertwilly <hebertwilly@hotmail.com>
2026-03-16 20:25:07 -0300

    fix: corrige o tratamento de erro adiciona o errorzod onde trata erro status 400

--- src/middlewares/error.middleware.ts
+++ src/middlewares/error.middleware.ts
@@ -1,21 +1,32 @@
 import { Request, Response, NextFunction } from "express";
+import { ZodError } from "zod";
 import { logger } from "../config/logger";
 
 export function errorMiddleware(
-  err: Error,
+  error: Error,
   req: Request,
   res: Response,
   next: NextFunction
 ) {
   logger.error({
-    message: err.message,
-    stack: err.stack,
+    message: error.message,
+    stack: error.stack,
     path: req.path,
-    method: req.method,
   });
 
+  if (error instanceof ZodError) {
+    return res.status(400).json({
+      success: false,
+      message: "Erro de validação",
+      errors: error.issues.map((issue) => ({
+        path: issue.path,
+        message: issue.message,
+      })),
+    });
+  }
+
   return res.status(500).json({
-    error: true,
-    message: err.message || "Erro interno do servidor",
+    success: false,
+    message: error.message || "Erro interno do servidor",
   });
 }
