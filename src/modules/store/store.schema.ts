e7ec2c3c2dece914d45be3abc22d2529df1097e3
hebertwilly <hebertwilly@hotmail.com>
2026-03-16 19:56:07 -0300

    feat: implementa schema para update store

--- src/modules/store/store.schema.ts
+++ src/modules/store/store.schema.ts
@@ -18,4 +18,30 @@ export const createStoreSchema = z.object({
     .min(10, "WhatsApp inválido"),
 });
 
+export const updateStoreSchema = z.object({
+  name: z
+    .string()
+    .min(3, "Nome deve ter pelo menos 3 caracteres")
+    .optional(),
+  email: z
+    .string()
+    .email("Email inválido")
+    .optional(),
+
+  password: z
+    .string()
+    .min(6, "Senha deve ter no mínimo 6 caracteres")
+    .optional(),
+
+  whatsappNumber: z
+    .string()
+    .min(10, "WhatsApp inválido")
+    .optional(),
+})
+.refine(
+    (data) => Object.keys(data).length > 0,
+    "Informe ao menos um campo para atualização"
+);
+
 export type CreateStoreDTO = z.infer<typeof createStoreSchema>;
+export type UpdateStoreDTO = z.infer<typeof updateStoreSchema>;
