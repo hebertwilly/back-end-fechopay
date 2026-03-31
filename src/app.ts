007da7ff545baa7dea2b5761bebbf88c8480f6c9
hebertwilly <hebertwilly@hotmail.com>
2026-03-31 20:29:29 -0300

    feat: adiciona o middleware cookieParser() em toda a aplicação

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import cookieParser from "cookie-parser";
 import { routes } from "./routes";
 import { errorMiddleware } from "./middlewares/error.middleware";
 
@@ -7,6 +8,7 @@ export const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api", routes);
 
