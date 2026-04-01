68f228b4fbcedf689fb0f2d11a93c6a2e01c94fe
hebertwilly <hebertwilly@hotmail.com>
2026-03-31 21:18:12 -0300

    feat: implementa a rota get do método auth/me

--- src/modules/auth/auth.routes.ts
+++ src/modules/auth/auth.routes.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import { authController } from "./auth.controller";
-
+import { authMiddleware } from "../../middlewares/auth.middleware";
 const authRoutes = Router();
 
 authRoutes.post("/login", authController.login.bind(authController));
+authRoutes.get("/me", authMiddleware, authController.me.bind(authController));
 
 export { authRoutes };
