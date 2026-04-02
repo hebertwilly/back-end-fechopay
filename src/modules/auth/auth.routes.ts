9ae44378c0483c44f6b6f7a391b9331e25f0b7ae
hebertwilly <hebertwilly@hotmail.com>
2026-04-01 21:35:54 -0300

    refactor: exclusão do bind das rotas, por não ser necessario o uso desse recurso

--- src/modules/auth/auth.routes.ts
+++ src/modules/auth/auth.routes.ts
@@ -3,8 +3,8 @@ import { authController } from "./auth.controller";
 import { authMiddleware } from "../../middlewares/auth.middleware";
 const authRoutes = Router();
 
-authRoutes.post("/login", authController.login.bind(authController));
-authRoutes.get("/me", authMiddleware, authController.me.bind(authController));
-authRoutes.post("/logout", authController.logout.bind(authController));
+authRoutes.post("/login", authController.login);
+authRoutes.get("/me", authMiddleware, authController.me);
+authRoutes.post("/logout", authController.logout);
 
 export { authRoutes };
